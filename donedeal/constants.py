516610e1046f731685701f91b3c8db6b2326848c
TWP24 <Fiachrah98@icloud.com>
2026-04-18 10:08:06 -0600

    Update constants.py

--- donedeal/constants.py
+++ donedeal/constants.py
@@ -1,6 +1,6 @@
 import re
 
-DOMAIN="https://www.donedeal.co.uk"
+DOMAIN="https://www.donedeal.ie"
 CARURL = "{}/cars".format(DOMAIN)
 REQUEST_HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
